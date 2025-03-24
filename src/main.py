2dca9367d9da146bb93604c554c4a2dd725dbc84
bindrakesh <rks.bind@gmail.com>
2025-03-24 17:56:06 +0530

    updated dockerfile for railway

--- src/main.py
+++ src/main.py
@@ -155,6 +155,6 @@ async def test():
 if __name__ == "__main__":
     import uvicorn
     import os
-    port = int(os.getenv("PORT", 5000))
+    port = int(os.getenv("PORT", 8080))
     uvicorn.run(app, host="0.0.0.0", port=port)
     
